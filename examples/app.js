9911ad786522739cc09ba57ebb2b163a4f2ed009
vicanso <vicansocanbico@gmail.com>
2016-08-17 23:17:22 +0800

    modify pie

--- examples/app.js
+++ examples/app.js
@@ -2,16 +2,55 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import Circle from '../lib/circle';
 import Pie from '../lib/pie';
+import Legend from '../lib/legend';
+import * as defaults from '../lib/defaults';
 
 class App extends Component {
   componentDidMount() {
+    this.renderLegendView();
+    this.renderCircleView();
+    this.renderPieView();
+  }
+  renderPieView() {
     const refs = this.refs;
+    const defaultPie = new Pie(refs.defaultPie);
+
+    defaultPie.render([
+      {
+        name: 'IE',
+        value: 18,
+      },
+      {
+        name: 'Firefox',
+        value: 23,
+      },
+      {
+        name: 'Chrome',
+        value: 84,
+      },
+      {
+        name: 'Edge',
+        value: 30,
+      },
+      {
+        name: 'Safari',
+        value: 30,
+      },
+      {
+        name: 'Opera',
+        value: 10,
+      },
+    ]);
+  }
+  renderCircleView() {
+    const refs = this.refs;
+
     const defaultCircle = new Circle(refs.defaultCircle);
-    defaultCircle.render(Math.random());
+    defaultCircle.render(0.543);
 
     const noneBGCircle = new Circle(refs.noneBGCircle);
     noneBGCircle.set('disabled.background', true);
-    noneBGCircle.render(Math.random());
+    noneBGCircle.render(0.6184);
 
     const liveUpdateCircle = new Circle(refs.liveUpdateCircle);
     liveUpdateCircle.render(Math.random());
@@ -20,64 +59,114 @@ class App extends Component {
     }, 1000);
 
     const changeFillColorCircle = new Circle(refs.changeFillColorCircle);
-    const newColor = 'RGB(67, 67, 72)';
-    changeFillColorCircle.set('style.foreground.fill', newColor);
-    changeFillColorCircle.set('style.label.fill', newColor);
-    changeFillColorCircle.render(Math.random());
+    const newColor = defaults.colors[1];
+    changeFillColorCircle.set('style.foreground.fill', newColor)
+      .set('style.label.fill', newColor)
+      .render(0.3458);
 
     const thicknessFullFillCircle = new Circle(refs.thicknessFullFillCircle);
     thicknessFullFillCircle
       .set({
         'disabled.background': true,
         'thickness': '100%',
-        'style.label.fill': 'RGB(67, 67, 72)',
+        'style.label.fill': defaults.colors[3],
       })
-      .render(Math.random());
+      .render(0.2458);
 
+    const halfCircle = new Circle(refs.halfCircle);
+    halfCircle.set({
+      startAngle: -Math.PI / 2,
+      endAngle: Math.PI / 2,
+      thickness: 20,
+    }).render(0.432);
+  }
+  renderLegendView() {
+    const refs = this.refs;
 
-    const defaultPie = new Pie(refs.defaultPie);
-    defaultPie.render([
+    const defaultLegend = new Legend(refs.defaultLegend);
+    defaultLegend.render([
       {
-        name: 'safari',
-        value: 18,
+        text: 'Chrome',
       },
       {
-        name: 'ie',
-        value: 54,
+        text: 'Firefox',
       },
       {
-        name: 'firefox',
-        value: 23,
+        text: 'Edge',
       },
       {
-        name: 'chrome',
-        value: 84,
+        text: 'Safari',
       },
       {
-        name: 'ios-safari',
-        value: 60,
+        text: 'IE',
       },
       {
-        name: 'uc',
-        value: 30,
-      },
-      {
-        name: 'opera',
-        value: 10,
+        text: 'Opera',
       },
     ]);
-    // defaultPie.render([{10, 20, 40]);
+    const mobileBrowserList = [
+      'iOS Safari',
+      'Opera Mini',
+      'Android Browser',
+      'Chrome for Android'
+    ];
+    const columnLegend = new Legend(refs.columnLegend);
+    columnLegend.set('type', 'column')
+      .render(mobileBrowserList);
 
-    // const maxCountPie = new Pie(refs.maxCountPie);
-    // maxCountPie.render([10, 30]);
+    const toggleLegend = new Legend(refs.toggleLegend);
+    toggleLegend.render(mobileBrowserList).on('click', index => {
+      toggleLegend.toggle(index);
+    });
+
+    const orderDisableLegend = new Legend(refs.orderDisableLegend);
+    let disabledIndex = -1;
+    orderDisableLegend.set('type', 'column')
+      .render(mobileBrowserList);
+    setInterval(() => {
+      orderDisableLegend.enable(disabledIndex);
+      disabledIndex = (disabledIndex + 1) % mobileBrowserList.length;
+      orderDisableLegend.disable(disabledIndex )
+    }, 1000);
   }
   render() {
     return (
       <div style={{
         paddingBottom: '20px',
       }}>
+        <h2>Legend Examples</h2>
+        <div className="pure-g">
+          <section className="pure-u-1-4 chartSection">
+            <h3>Default</h3>
+            <svg
+              ref="defaultLegend"
+            >
+            </svg>
+          </section>
+          <section className="pure-u-1-4 chartSection">
+            <h3>Column</h3>
+            <svg
+              ref="columnLegend"
+            >
+            </svg>
+          </section>
+          <section className="pure-u-1-4 chartSection">
+            <h3>Toggle</h3>
+            <svg
+              ref="toggleLegend"
+            >
+            </svg>
+          </section>
+          <section className="pure-u-1-4 chartSection">
+            <h3>Disable One By One</h3>
+            <svg
+              ref="orderDisableLegend"
+            >
+            </svg>
+          </section>
+        </div>
         <h2>Circle Examples</h2>
-        <div className="pure-g hidden">
+        <div className="pure-g">
           <section className="pure-u-1-4 chartSection">
             <h3>Default</h3>
             <svg
@@ -113,6 +202,13 @@ class App extends Component {
             >
             </svg>
           </section>
+          <section className="pure-u-1-4 chartSection">
+            <h3>Half</h3>
+            <svg
+              ref="halfCircle"
+            >
+            </svg>
+          </section>
         </div>
         <h2>Pie Examples</h2>
         <div className="pure-g">
@@ -120,13 +216,6 @@ class App extends Component {
             <h3>Default</h3>
             <svg
               ref="defaultPie"
-            >
-            </svg>
-          </section>
-          <section className="pure-u-1-4 chartSection">
-            <h3>Set Max Count</h3>
-            <svg
-              ref="maxCountPie"
             >
             </svg>
           </section>
